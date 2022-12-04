37bb23e0f26307e061fdec25bdddf783f0baccd8
orlandopalmeira <orlandopalmeira51@gmail.com>
2022-12-04 21:09:20 +0000

    Completei a classe Menu

--- Terceira fase/src/ui/Menu.java
+++ Terceira fase/src/ui/Menu.java
@@ -1,5 +1,143 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Menu{
     
+    public interface Handler{
+        void execute();
+    }
+
+    public interface PreCondition{
+        boolean validate();
+    }
+
+    private static Scanner is = new Scanner(System.in);
+
+    private String titulo;
+    private List<String> opcoes;
+    private List<PreCondition> disponivel;
+    private List<Handler> handlers;
+
+    private Menu(){}
+
+    public Menu(String titulo, List<String> opcoes){
+        this.titulo = titulo;
+        this.opcoes = opcoes = new ArrayList<String>(opcoes);
+        this.disponivel = new ArrayList<PreCondition>();
+        this.handlers = new ArrayList<Handler>();
+        this.opcoes.forEach(s -> {
+            this.disponivel.add(() -> true);
+            this.handlers.add(() -> System.out.println("\nATENÇÃO: Opção não implementada!"));
+        });
+    }
+
+    public Menu(String titulo, String[] opcoes){
+        this(titulo,Arrays.asList(opcoes));
+    }
+
+    /**
+     * Adicionar opções a um Menu.
+     *
+     * @param name A opção a apresentar.
+     * @param p A pré-condição da opção.
+     * @param h O event handler para a opção.
+     */
+    public void option(String name, PreCondition p, Handler h) {
+        this.opcoes.add(name);
+        this.disponivel.add(p);
+        this.handlers.add(h);
+    }
+
+    /**
+     * Correr o menu uma vez.
+     */
+    public void runOnce() {
+        int op;
+        show();
+        op = readOption();
+        // testar pré-condição
+        if (op>0 && !this.disponivel.get(op-1).validate()) {
+            System.out.println("Opção indisponível!");
+        } else if (op>0) {
+            // executar handler
+            this.handlers.get(op-1).execute();
+        }
+    }
+
+    /**
+     * Correr o menu multiplas vezes.
+     *
+     * Termina com a opção 0 (zero).
+     */
+    public void run() {
+        int op;
+        do {
+            show();
+            op = readOption();
+            // testar pré-condição
+            if (op>0 && !this.disponivel.get(op-1).validate()) {
+                System.out.println("Opção indisponível! Tente novamente.");
+            } else if (op>0) {
+                // executar handler
+                this.handlers.get(op-1).execute();
+            }
+        } while (op != 0);
+    }
+
+    /**
+     * Método que regista uma uma pré-condição numa opção do menu.
+     *
+     * @param i índice da opção (começa em 1)
+     * @param b pré-condição a registar
+     */
+    public void setPreCondition(int i, PreCondition b) {
+        this.disponivel.set(i-1,b);
+    }
+
+    /**
+     * Método para registar um handler numa opção do menu.
+     *
+     * @param i indice da opção  (começa em 1)
+     * @param h handlers a registar
+     */
+    public void setHandler(int i, Handler h) {
+        this.handlers.set(i-1, h);
+    }
+
+    // Métodos auxiliares
+
+    /** Apresentar o menu */
+    private void show() {
+        System.out.println("\n *** "+this.titulo+" *** ");
+        for (int i=0; i<this.opcoes.size(); i++) {
+            System.out.print(i+1);
+            System.out.print(" - ");
+            System.out.println(this.disponivel.get(i).validate()?this.opcoes.get(i):"---");
+        }
+        System.out.println("0 - Sair");
+    }
+
+    /** Ler uma opção válida */
+    private int readOption() {
+        int op;
+
+        System.out.print("Opção: ");
+        try {
+            String line = is.nextLine();
+            op = Integer.parseInt(line);
+        }
+        catch (NumberFormatException e) { // Não foi inscrito um int
+            op = -1;
+        }
+        if (op<0 || op>this.opcoes.size()) {
+            System.out.println("Opção Inválida!!!");
+            op = -1;
+        }
+        return op;
+    }
+
 }
